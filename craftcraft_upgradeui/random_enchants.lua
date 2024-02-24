2f0fa9882208142bffb268f3ff33f59be6a6b193
Aviv Tokman <avivtokman@gmail.com>
2024-02-24 15:17:39 +0100

    [feat] added simple tooltips to template

--- craftcraft_upgradeui/random_enchants.lua
+++ craftcraft_upgradeui/random_enchants.lua
@@ -13,4 +13,17 @@ end
 MainFrame:SetScript("OnShow", function()
     local tex = GetItemIcon(GetLink())
     SetPortraitToTexture("CCREnchantFramePortrait", tex);
+    MainFrame.link = GetLink();
+
+
+    -- play here
+    CCREnchantFrameSection1IconBorder:SetVertexColor(GetItemQualityColor(3))
+    CCREnchantFrameSection2IconBorder:SetVertexColor(GetItemQualityColor(4))
+    CCREnchantFrameSection3IconBorder:SetVertexColor(GetItemQualityColor(5))
+    CCREnchantFrameSection2IconTexture:SetTexture("Interface\\Icons\\spell_shadow_sealofkings")
+    CCREnchantFrameSection1IconTexture:SetTexture("Interface\\Icons\\ability_mage_missilebarrage")
+    CCREnchantFrameSection3IconTexture:SetTexture("Interface\\Icons\\spell_nature_naturesblessing")
+    CCREnchantFrameSection1.enchant = 155
+    CCREnchantFrameSection2.enchant = 156
+    CCREnchantFrameSection3.enchant = 157
 end)
